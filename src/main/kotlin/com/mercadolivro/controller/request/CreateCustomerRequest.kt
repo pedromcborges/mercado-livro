97eb6167b3103a893fcb6425e17fab144283f567
Pedro Borges <pedromcborges11@gmail.com>
2022-03-14 18:44:56 +0000

    validating if email available with custom validator

--- src/main/kotlin/com/mercadolivro/controller/request/CreateCustomerRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/CreateCustomerRequest.kt
@@ -2,6 +2,7 @@ package com.mercadolivro.controller.request
 
 import com.mercadolivro.enums.CustomerStatus
 import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.validation.EmailAvailable
 import javax.validation.constraints.Email
 import javax.validation.constraints.NotEmpty
 
@@ -10,12 +11,14 @@ data class CreateCustomerRequest(
     var name: String,
 
     @field:Email(message = "{email.invalid}")
+    @EmailAvailable(message = "{email.already.used}")
     var email: String
 ) {
     fun toCustomerModel(): CustomerModel {
         return CustomerModel(
             name = this.name,
             email = this.email,
-            status = CustomerStatus.ACTIVE)
+            status = CustomerStatus.ACTIVE
+        )
     }
 }
